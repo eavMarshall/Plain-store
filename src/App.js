8937cd18cc43a76dddb7405d6ac4e18bfc551185
eavMarshall <elliottavmarshall@gmail.com>
2023-02-23 03:43:49 +0700

    Adding input example

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import AppStore from "src/AppStore";
 const useAppStore = AppStore.useStore();
 
 const App = () => {
+    const input = useAppStore(state => state.input);
     const key1 = useAppStore(state => state.key1);
     const key2 = useAppStore(state => state.key2);
     const key3 = useAppStore(state => state.key3);
 
     return <div>
-        <h1>Hello world</h1>
+        <h1>{input}</h1>
 
+        <input value={input} onChange={event => AppStore.setState({input: event.target.value})}></input>
+        <br/>
+
+        {/* use store functions*/}
         <button onClick={() => AppStore.setState({key1: "I'm clicked!!"})}>Key1 = {key1}</button>
         <br/>
         <button onClick={() => AppStore.setState({key2: "I'm clicked!!"})}>Key2 = {key2}</button>
         <br/>
         <button onClick={() => AppStore.setState({key3: "I'm clicked!!"})}>Key3 = {key3}</button>
         <br/>
+        {/*use AppStore functions*/}
         <button onClick={() => AppStore.setKey1("I'm clicked!!")}>Key1 = {key1}</button>
         <br/>
         <button onClick={() => AppStore.setKey2("I'm clicked!!")}>Key2 = {key2}</button>
--- src/AppStore.js
+++ src/AppStore.js
@@ -1,6 +1,7 @@
 import createStore from "src/common/createStore";
 
 const initState = {
+    input: "Hello world!",
     key1: 'key1',
     key2: 'key2',
     key3: 'key3',
